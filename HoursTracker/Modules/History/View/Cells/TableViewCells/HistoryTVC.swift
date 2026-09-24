f5d6d44ae008c0fc1d2a192039d8a57d6a01da37
H A M I . <hamiddot47@gmail.com>
2026-09-24 04:49:13 +0100

    Created total shifts card view and shifts detail table view.

---
+++ HoursTracker/Modules/History/View/Cells/TableViewCells/HistoryTVC.swift
@@ -0,0 +1,23 @@
+//
+//  CalenderTVC.swift
+//  HoursTracker
+//
+//  Created by H A M I . on 24/09/2026.
+//
+
+import UIKit
+
+class HistoryTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
